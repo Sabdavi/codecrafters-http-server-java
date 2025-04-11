c90234f52d55a618153d260c5151e7d8e559e341
saeid <saeed@mediadistillery.com>
2025-04-11 13:10:08 +0200

    add 200 response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,33 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        // Uncomment this block to pass the first stage
+        //
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            Socket accept = serverSocket.accept();// Wait for connection from client.
+            try(OutputStream outputStream = accept.getOutputStream()) {
+                PrintWriter writer = new PrintWriter(outputStream, true);
+                writer.println("HTTP/1.1 200 OK\\r\\n\\r\\n");
+            }
+
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+
+        }
+    }
 }
