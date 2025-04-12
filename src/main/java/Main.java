bd07ca0d5c9a88ba0144065723930938d3d12f82
saeid <saeed@mediadistillery.com>
2025-04-12 18:59:19 +0200

    bug bux

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,8 +21,6 @@ public class Main {
             if (requestPath.startsWith("/echo/")) {
                 String[] pathElements = requestPath.split("/");
                 sendResponse(clientSocket, 200,Optional.of(pathElements[2]));
-            } else {
-                sendResponse(clientSocket, 404, Optional.empty());
             }
         } catch (IOException e) {
             serverSocket.close();
