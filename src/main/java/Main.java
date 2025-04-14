878793fb1b0221d3cb98541d0ad8425eb3643afd
saeid <saeed@mediadistillery.com>
2025-04-14 21:12:53 +0200

    add user-agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,10 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Main {
+    private static final String USER_AGENT = "User-Agent";
     public static void main(String[] args) throws IOException {
         System.out.println("Logs from your program will appear here!");
         ServerSocket serverSocket = null;
@@ -17,14 +15,19 @@ public class Main {
             Socket clientSocket = serverSocket.accept();
             System.out.println("accepted new connection");
 
-            String requestPath = readReadPath(clientSocket);
+            List<String> requestData = readRequestData(clientSocket);
+            String requestPath = readReadPath(requestData);
             if(requestPath.equals("/")) {
                 sendResponse(clientSocket, 200,Optional.empty());
             }
             if (requestPath.startsWith("/echo/")) {
                 String[] pathElements = requestPath.split("/");
                 sendResponse(clientSocket, 200,Optional.of(pathElements[2]));
-            } else {
+            } if (requestPath.startsWith("/user-agent/")) {
+                Map<String, String> headers = readHeaders(requestData);
+                sendResponse(clientSocket, 200, Optional.of(headers.get(USER_AGENT)));
+            }
+            else {
                 sendResponse(clientSocket, 404, Optional.empty());
             }
         } catch (IOException e) {
@@ -34,10 +37,19 @@ public class Main {
         }
     }
 
-    private static String readReadPath(Socket clientSocket) throws IOException {
-        List<String> requestData = readRequestData(clientSocket);
+    private static String readReadPath(List<String> requestData) throws IOException {
         List<String> requestLine = Arrays.stream(requestData.getFirst().split(" ")).toList();
         return requestLine.get(1);
+    }
+
+    private static Map<String, String> readHeaders(List<String> requestData) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        for(int i=1 ; i < requestData.size() ; i++) {
+            String header = requestData.get(i);
+            String[] split = header.split(":");
+            headers.put(split[0], split[1]);
+        }
+        return headers;
     }
 
     private static List<String> readRequestData(Socket clientSocket) throws IOException {
