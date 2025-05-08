f5a098728bebcf624b3ddca7057e7a4cc55e9d69
Sabdavi <abdavi.saeed@gmail.com>
2025-05-08 17:09:50 +0200

    add file processing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,10 +14,20 @@ public class Main {
     private static final String CRLF = "\r\n";
     private static final ExecutorService executorService = Executors.newCachedThreadPool();
     private static final int PORT_NUMBER = 4221;
+    private static String FILE_DIR;
+    private static String METHOD_KEY = "METHOD";
+    private static String TARGET_KEY = "TARGET";
+    private static String VERSION_KEY = "VERSION";
+    private static int HTTP_STATUS_OK = 200;
+    private static int HTTP_STATUS_NOT_FOUND = 404;
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Logs from your program will appear here!");
+        parseEnvs(args);
+        createFileDirectory(FILE_DIR);
+        createIntialFile(FILE_DIR, "foo");
+        System.out.printf("use %s as file directory%n", FILE_DIR);
+
         try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)) {
             serverSocket.setReuseAddress(true);
 
@@ -25,6 +35,31 @@ public class Main {
                 Socket clientSocket = serverSocket.accept();
                 processRequest(clientSocket);
             }
+        }
+    }
+
+    private static void createFileDirectory(String fileDir) throws IOException {
+        File dir = new File(fileDir);
+        if (!dir.exists() && !dir.mkdirs()) {
+                throw new IOException("Failed to create directories: " + fileDir);
+            }
+    }
+
+    private static void createIntialFile(String fileDir, String fileName) throws IOException {
+        File file = new File(String.format("%s%s",fileDir, fileName));
+        if (file.exists()) {
+            file.delete();
+        }
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write("Hello, World!");
+        }
+    }
+
+    private static void parseEnvs(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("No file directory provided");
+        } else {
+            FILE_DIR = args[0];
         }
     }
 
@@ -55,7 +90,10 @@ public class Main {
                 } else if (requestPath.startsWith("/user-agent")) {
                     Map<String, String> headers = readHeaders(requestData);
                     sendResponse(clientSocket, 200, Optional.of(headers.get(USER_AGENT)));
-                } else {
+                } else if (requestPath.startsWith("/files/")) {
+                    processFileRequest(requestData);
+                }
+                else {
                     sendResponse(clientSocket, 404, Optional.empty());
                 }
             } catch (IOException e) {
@@ -68,64 +106,83 @@ public class Main {
                 }
             }
         }
-    }
 
-    private static Map<String, String> parseRequestLine(List<String> requestData) throws IOException {
-        Map<String, String> requestLine = new HashMap<>();
-        List<String> requestLineElements = Arrays.stream(requestData.getFirst().split(" ")).toList();
-        requestLine.put("METHOD", requestLineElements.get(0));
-        requestLine.put("TARGET", requestLineElements.get(1));
-        requestLine.put("VERSION", requestLineElements.get(2));
-        return requestLine;
-    }
+        private void processFileRequest(List<String> requestData) throws IOException {
+            String target = parseRequestLine(requestData).get(TARGET_KEY);
+            String[] split = target.split("/");
+            String fileName = split[2];
+            String content;
 
-    private static Map<String, String> readHeaders(List<String> requestData) throws IOException {
-        Map<String, String> headers = new HashMap<>();
-        for (int i = 1; i < requestData.size(); i++) {
-            String header = requestData.get(i);
-            String[] split = header.split(":");
-            headers.put(split[0].trim(), split[1].trim());
+            if(fileName.equals("foo")) {
+                File file = new File(String.format("%s%s",FILE_DIR, fileName));
+                try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String inputLine;
+                    List<String> inputLines = new ArrayList<>();
+                    while ((inputLine = bufferedReader.readLine()) != null) {
+                        inputLines.add(inputLine);
+                    }
+                    content = String.join("", inputLines);
+                }
+                sendResponse(clientSocket, HTTP_STATUS_OK, Optional.of(content) );
+            }
+            else {
+                sendResponse(clientSocket, HTTP_STATUS_NOT_FOUND, Optional.empty() );
+            }
         }
-        return headers;
-    }
 
-    private static List<String> readRequestData(Socket clientSocket) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine;
-        List<String> requestData = new ArrayList<>();
-        while (!(inputLine = reader.readLine()).isEmpty())
-            requestData.add(inputLine);
-        return requestData;
-    }
+        private static Map<String, String> parseRequestLine(List<String> requestData) {
+            Map<String, String> requestLine = new HashMap<>();
+            List<String> requestLineElements = Arrays.stream(requestData.getFirst().split(" ")).toList();
+            requestLine.put(METHOD_KEY, requestLineElements.get(0));
+            requestLine.put(TARGET_KEY, requestLineElements.get(1));
+            requestLine.put(VERSION_KEY, requestLineElements.get(2));
+            return requestLine;
+        }
 
-    private static void sendResponse(Socket clientSocket, int statusCode, Optional<String> body) throws IOException {
-        String status = buildStatusString(statusCode);
-        OutputStream outputStream = clientSocket.getOutputStream();
-        PrintWriter writer = new PrintWriter(outputStream);
-        writer.write(String.format("%s %s %s", HTTP_VERSION, status, CRLF));
-        if (body.isPresent()) {
-            String bodyContent = body.get();
-            int length = bodyContent.getBytes().length;
-            writer.write(String.format("%s : %d %s", CONTENT_LENGTH, length, CRLF));
-            writer.write(String.format("%s : text/plain %s", CONTENT_TYPE, CRLF));
+        private static Map<String, String> readHeaders(List<String> requestData) throws IOException {
+            Map<String, String> headers = new HashMap<>();
+            for (int i = 1; i < requestData.size(); i++) {
+                String header = requestData.get(i);
+                String[] split = header.split(":");
+                headers.put(split[0].trim(), split[1].trim());
+            }
+            return headers;
+        }
+
+        private static List<String> readRequestData(Socket clientSocket) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            List<String> requestData = new ArrayList<>();
+            while (!(inputLine = reader.readLine()).isEmpty())
+                requestData.add(inputLine);
+            return requestData;
+        }
+
+        private static void sendResponse(Socket clientSocket, int statusCode, Optional<String> body) throws IOException {
+            String status = buildStatusString(statusCode);
+            OutputStream outputStream = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(outputStream);
+            writer.write(String.format("%s %s %s", HTTP_VERSION, status, CRLF));
+            if (body.isPresent()) {
+                String bodyContent = body.get();
+                int length = bodyContent.getBytes().length;
+                writer.write(String.format("%s : %d %s", CONTENT_LENGTH, length, CRLF));
+                writer.write(String.format("%s : text/plain %s", CONTENT_TYPE, CRLF));
+                writer.write(CRLF);
+                writer.write(body.get());
+                writer.write(CRLF);
+            }
             writer.write(CRLF);
-            writer.write(body.get());
-            writer.write(CRLF);
+            writer.flush();
         }
-        writer.write(CRLF);
-        writer.flush();
-    }
 
-    private static String buildStatusString(int statusCode) {
-        if (statusCode == 200) {
-            return "200 OK";
-        } else if (statusCode == 404) {
-            return "404 Not Found";
+        private static String buildStatusString(int statusCode) {
+            if (statusCode == 200) {
+                return "200 OK";
+            } else if (statusCode == 404) {
+                return "404 Not Found";
+            }
+            return "";
         }
-        return "";
-    }
-
-    private void test() {
-
     }
 }
