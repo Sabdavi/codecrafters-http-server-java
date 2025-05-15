c6738365aacf6503877bfe4b9fd168e6e785f16b
Sabdavi <abdavi.saeed@gmail.com>
2025-05-15 11:40:50 +0200

    codecrafters submit [skip ci]

---
+++ src/main/java/com/saeid/HttpStatus.java
@@ -0,0 +1,44 @@
+package com.saeid;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    NOT_FOUND(401, "Not Found");
+
+    private static final HttpStatus[] VALUES;
+
+    static {
+        VALUES = values();
+    }
+
+    private final int value;
+    private final String reason;
+
+    HttpStatus(int value, String reason) {
+        this.value = value;
+        this.reason = reason;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public static HttpStatus valueOf(int statusCode) {
+        for (HttpStatus status : values()) {
+            if (status.value == statusCode) {
+                return status;
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.value +" "+this.reason;
+    }
+}
